7a0aae9d842c311576242640a988adbb381b1593
Jordy van Kuijk <jvankuijk@me.com>
2014-02-20 22:50:39 +0100

    changed the algorithm to adhere with Fibonacci sequence

--- angular-fibonacci.js
+++ angular-fibonacci.js
@@ -1,4 +1,4 @@
-'use strict';
+	'use strict';
 
 angular.module('angular-fibonacci').factory('Fibonacci', ['$timeout', function($timeout) {
 	return  {
@@ -8,12 +8,15 @@ angular.module('angular-fibonacci').factory('Fibonacci', ['$timeout', function($
 		next: 1,
 		max: 5,
 		initialize: function(options) {
-			this.series = [1, 1];
+			this.series = [1];
 			this.index = 0;
 			this.next = 1;
 
-			for(var prop in options) {
-				this[prop] = options[prop];
+			if(options && options.max !== undefined) {
+				this.max = options.max;
+			} 
+			if(options && options.callback !== undefined) {
+				this.callback = options.callback;
 			}
 		},
 		start: function (options) {
@@ -31,12 +34,19 @@ angular.module('angular-fibonacci').factory('Fibonacci', ['$timeout', function($
 		tick: function(seconds) {
 			var self = this;
 			self.timeout = $timeout(function(){
-				self.next = self.series[self.index] + self.series[self.index + 1];
+
+				// only if we have more than one entry in the series, take the last two entries
+				if(self.series.length > 1) {
+					self.next = self.series[self.index] + self.series[self.index + 1];
+				}
 				self.series.push(self.next);
+
 				if(self.callback && typeof(self.callback) == "function") {
 					self.callback();
 				}
 				self.index ++;
+
+				// if we passed the maximum number of items
 				if(self.series.length > self.max) {
 					self.restart();
 				} else {
